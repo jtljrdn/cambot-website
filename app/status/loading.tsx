e6cdf0a124dfc7368898784188fbf68dbe233f74
Jordan Lee <jtlee@rmgj2.com>
2024-01-22 12:30:42 -0600

    Update loading and status pages, add getStatus utility function

--- app/status/loading.tsx
+++ app/status/loading.tsx
@@ -4,11 +4,11 @@ const Loading = () => {
       <div className="text-6xl text-center font-bold my-[4rem]">Status</div>
       <div className="rounded-box bg-base-200 mb-10 flex justify-center">
         <div className="flex flex-col gap-4 max-w-[56rem] my-5 items-center px-10">
-          <div className="skeleton w-32 h-6"></div>
-          <div className="skeleton w-28 h-4"></div>
-          <div className="skeleton w-28 h-4"></div>
-          <div className="skeleton w-28 h-4"></div>
-          <div className="skeleton w-28 h-4"></div>
+          <div className="skeleton w-80 h-10"></div>
+          <div className="skeleton w-48 h-4"></div>
+          <div className="skeleton w-48 h-4"></div>
+          <div className="skeleton w-48 h-4"></div>
+          <div className="skeleton w-48 h-4"></div>
         </div>
       </div>
     </div>
