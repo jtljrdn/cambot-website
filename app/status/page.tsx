84dd8609527ec7a1950aabe0fd5c5f0f46d6ee48
Jordan Lee <jtlee@rmgj2.com>
2024-01-22 18:59:10 -0600

    Update margin-bottom in status page

--- app/status/page.tsx
+++ app/status/page.tsx
@@ -11,7 +11,7 @@ const status = async () => {
   return (
     <div>
       <StatusCard />
-      <div className="text-center py-5 bg-base-200 rounded-box justify-center mb-[6rem]">
+      <div className="text-center py-5 bg-base-200 rounded-box justify-center mb-[8rem]">
         <p className="text-3xl font-bold mb-2">Have a problem?</p>
         <Link href="https://discord.gg/bDwKqSreue">
           <p className="btn btn-primary text-2xl font-bold">Let Us Know.</p>
